857e9d76083d6c12f58714c1ea10a20fdf00ed11
KmNeetuSingh <itisneetusingh@gmail.com>
2025-09-21 20:15:51 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+connectDB();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Server health is good");
+});
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
