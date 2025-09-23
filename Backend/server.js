ca382261b6b4b1ddb96e9fd776c66d5ffbf1cc44
KmNeetuSingh <itisneetusingh@gmail.com>
2025-09-23 20:19:07 +0530

    deleted

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config({override:true});
 const express = require("express");
 const connectDB = require("./config/db");
 connectDB();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.get("/", (req, res) => {
   res.send("Server health is good");
